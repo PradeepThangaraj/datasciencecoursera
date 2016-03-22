5646a9249c96740a2320b6428139a4f4bce11beb
PradeepThangaraj <pradeep.thangaraj@gmail.com>
2016-03-22 15:39:58 -0400

    Added files via upload

---
+++ solveMatrix.R
@@ -0,0 +1,27 @@
+# Below is the function to create a matrix and provide a square matrix as input
+makeCacheMatrix <- function(x = matrix()) {
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) inv <<- inverse
+  getinverse <- function() inv
+  list(set = set,
+       get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
+}
+# Below is the function to get the inverse of the matrix
+cachesolve <- function(x, ...) {
+  m <- x$getinverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinverse(m)
+  m
+}
